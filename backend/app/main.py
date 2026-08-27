016e93eae884fc541f2a568bc09c3ae7a612452d
Sadnan Nishthup <nishchup489@gmail.com>
2026-08-27 19:16:08 -0400

    little change

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,7 +28,7 @@ if FRONTEND_API_URL:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allowed_origins,
+    allowed_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
